51c670e044ba4eff3444be27d85bc8e422d522bb
Xaver Köppl <x.koeppl@campus.lmu.de>
2025-12-25 16:30:47 +0100

    Die Nebenbedingung für affine Funktionen in SSC hinzugefügt.

--- Affine.py
+++ Affine.py
@@ -2,12 +2,14 @@ import numpy as np
 
 class AffineToLinear():
     @staticmethod
-    def makeLinear(x: np.ndarray):
+    #notwendige Transformation, um affine Subspaces in lineare Subspaces im erweiterten Raum zu überführen
+    def makeLinear(x: np.ndarray) -> np.ndarray:
         ones = np.ones((1, x.shape[1]))
         x_linear = np.vstack((x, ones))
         return x_linear
     
-    def normalize(x: np.ndarray):
+    @staticmethod
+    def normalize(x: np.ndarray) -> np.ndarray:
         x_norm = x / np.linalg.norm(x, axis=0, keepdims=True)
         return x_norm
 
--- subspace_clustering/cluster/selfrepresentation.py
+++ subspace_clustering/cluster/selfrepresentation.py
@@ -159,8 +159,9 @@ def active_support_elastic_net(X, y, alpha, tau=1.0, algorithm='spams', support_
             cs = sparse_encode(y, Xs, algorithm=algorithm, alpha=alpha)
       
         delta = (y - np.dot(cs, Xs)) / alpha
-		
-        obj = tau * np.sum(np.abs(cs[0])) + (1.0 - tau)/2.0 * np.sum(np.power(cs[0], 2.0)) + alpha/2.0 * np.sum(np.power(delta, 2.0))
+        gamma = 10 * alpha
+        
+        obj = tau * np.sum(np.abs(cs[0])) + (1.0 - tau)/2.0 * np.sum(np.power(cs[0], 2.0)) + alpha/2.0 * np.sum(np.power(delta, 2.0)) + gamma/2.0 * (np.sum(cs[0] - 1.0)) ** 2
         if curr_obj - obj < 1.0e-10 * curr_obj:
             break
         curr_obj = obj
