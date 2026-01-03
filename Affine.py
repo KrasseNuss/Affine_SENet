dafc4e62115e96e6ea54f8888183827f317426f7
Xaver Köppl <x.koeppl@campus.lmu.de>
2026-01-03 15:40:34 +0100

    Fubktion für SENet hinzugefügt.

--- Affine.py
+++ Affine.py
@@ -3,8 +3,16 @@ import numpy as np
 class AffineToLinear():
     @staticmethod
     #notwendige Transformation, um affine Subspaces in lineare Subspaces im erweiterten Raum zu überführen
+    #Für SSC
     def makeLinear(x: np.ndarray) -> np.ndarray:
         ones = np.ones((1, x.shape[1]))
+        x_linear = np.vstack((x, ones))
+        return x_linear
+    
+    #Für SENet
+    @staticmethod
+    def makeLinearSENet(x: np.ndarray) -> np.ndarray:
+        ones = np.ones((x.shape[1], 1))
         x_linear = np.vstack((x, ones))
         return x_linear
     
