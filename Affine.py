27d1a556ad747260e89e2bf0f547bd318c6a882b
Xaver Köppl <x.koeppl@campus.lmu.de>
2026-01-17 17:44:03 +0100

    Unnötige Methoden entfernt.

--- Affine.py
+++ Affine.py
@@ -3,21 +3,8 @@ import numpy as np
 class AffineToLinear():
     @staticmethod
     #notwendige Transformation, um affine Subspaces in lineare Subspaces im erweiterten Raum zu überführen
-    #Für SSC
     def makeLinear(x: np.ndarray) -> np.ndarray:
-        ones = np.ones((1, x.shape[1]))
-        x_linear = np.vstack((x, ones))
+        ones = np.ones((x.shape[0], 1))
+        x_linear = np.hstack((x, ones))
         return x_linear
     
-    #Für SENet
-    @staticmethod
-    def makeLinearSENet(x: np.ndarray) -> np.ndarray:
-        ones = np.ones((x.shape[1], 1))
-        x_linear = np.vstack((x, ones))
-        return x_linear
-    
-    @staticmethod
-    def normalize(x: np.ndarray) -> np.ndarray:
-        x_norm = x / np.linalg.norm(x, axis=0, keepdims=True)
-        return x_norm
-
