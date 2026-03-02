7421d2e3e552f62389edca4ab08a87d453b6e9d3
Xaver Köppl <x.koeppl@campus.lmu.de>
2026-03-02 11:27:59 +0100

    Eine Funktion main gebaut, damit man diese in einem Testskript verwenden kann.

--- SSC.py
+++ SSC.py
@@ -6,51 +6,90 @@ from sklearn.metrics import normalized_mutual_info_score, adjusted_rand_score
 from self_expressive_network.metrics.cluster.accuracy import clustering_accuracy
 import numpy as np
 from Affine import AffineToLinear as alt
+import time
+import random
 
 #Import aus Repo
 from subspace_clustering.cluster.selfrepresentation import ElasticNetSubspaceClustering
 
-test = True
-# generate 7 data points from 3 independent subspaces as columns of data matrix X
-#X = np.array([[1.0, -1.0, 0.0, 0.0, 0.0,  0.0, 0.0],
-#              [1.0,  0.5, 0.0, 0.0, 0.0,  0.0, 0.0],
-#              [0.0,  0.0, 1.0, 0.2, 0.0,  0.0, 0.0],
-#              [0.0,  0.0, 0.2, 1.0, 0.0,  0.0, 0.0],
-#              [0.0,  0.0, 0.0, 0.0, 1.0, -1.0, 0.0],
-#              [0.0,  0.0, 0.0, 0.0, 1.0,  1.0, -1.0]])
+def random_gamma():
+    gamma = np.random.uniform(0.1, 100)
+    return gamma
 
-cluster = np.loadtxt("/mnt/d/Xaver Köppl/Uni/Bachelorarbeit/git/SubCluGen/subspace_cluster.csv", delimiter=",", dtype=np.float64)
-labels = np.loadtxt("/mnt/d/Xaver Köppl/Uni/Bachelorarbeit/git/SubCluGen/subspace_lables.csv", delimiter=",", dtype=np.float64)
-labels = np.max(labels, axis=1)
-#model = ElasticNetSubspaceClustering(n_clusters=1,algorithm='lasso_lars',gamma=50).fit(A.T)
-#print(model.labels_)
-# this should give you array([1, 1, 0, 0, 2, 2, 2]) or a permutation of these labels
-#print("Data: ")
-#print(cluster)
-#print("\n")
-affine_model = alt.makeLinear(cluster)
-print("Model: ")
-print(affine_model)
-print("\n")
-model_affine = ElasticNetSubspaceClustering(n_clusters=2,algorithm='lasso_lars',gamma=10).fit(affine_model)
-print("Labels: ")
-print(labels)
-print("Labels: ")
-print(model_affine.labels_)
-if test:
-    print("-------------------------------------------")
-    print("cluster shape:", cluster.shape)
-    print("Labels Shape: ", labels.shape)
-    print("affine_model shape:", affine_model.shape)
-    print("predicted labels length:", len(model_affine.labels_))
-    print("affine Labels length:", model_affine.labels_.shape)
-    print("true labels length:", len(labels))
-    print("-------------------------------------------")
-#labels = np.max(labels, axis=1)
-ari = adjusted_rand_score(labels, model_affine.labels_)
-nmi = normalized_mutual_info_score(labels, model_affine.labels_, average_method='arithmetic')
-acc = clustering_accuracy(labels, model_affine.labels_)
-print("ARI: {:.4f}".format(ari))
-print("NMI: {:.4f}".format(nmi))
-print("ACC: {:.4f}".format(acc))
-print("\n")
+def same_seeds(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    print(f"Seeds set to: {seed}")
+
+def run_experiment(gamma, affine_model, cluster, labels,test):
+    if test:
+        print("Model: ")
+        print(affine_model)
+        print("\n")
+    model_affine = ElasticNetSubspaceClustering(n_clusters=2,algorithm='lasso_lars',gamma=gamma).fit(affine_model)
+    if test:
+        print("Labels: ")
+        print(labels)
+        print("Labels: ")
+        print(model_affine.labels_)
+        print("-------------------------------------------")
+        print("cluster shape:", cluster.shape)
+        print("Labels Shape: ", labels.shape)
+        print("affine_model shape:", affine_model.shape)
+        print("predicted labels length:", len(model_affine.labels_))
+        print("affine Labels length:", model_affine.labels_.shape)
+        print("true labels length:", len(labels))
+        print("-------------------------------------------")
+
+    ari = adjusted_rand_score(labels, model_affine.labels_)
+    nmi = normalized_mutual_info_score(labels, model_affine.labels_, average_method='arithmetic')
+    acc = clustering_accuracy(labels, model_affine.labels_)
+    return ari, nmi, acc
+
+def main():
+    test = False
+    start = time.perf_counter()
+    cluster = np.loadtxt("/mnt/d/Xaver Köppl/Uni/Bachelorarbeit/git/SubCluGen/subspace_cluster.csv", delimiter=",", dtype=np.float64)
+    labels = np.loadtxt("/mnt/d/Xaver Köppl/Uni/Bachelorarbeit/git/SubCluGen/subspace_lables.csv", delimiter=",", dtype=np.float64)
+    labels = np.max(labels, axis=1)
+    #print("Data: ")
+    #print(cluster)
+    affine_model = alt.makeLinear(cluster)
+
+    trial_num = 50
+    best_score = -1
+    best_gamma = None
+    for trial in range(trial_num):
+        gamma = random_gamma()
+        same_seeds(int(gamma) + trial)
+        print(f"Trial {trial} with gamma={gamma:.4f}")
+
+        ari, nmi, acc = run_experiment(gamma, affine_model, cluster, labels, test)
+
+        if ari > best_score:
+            best_score = ari
+            best_gamma = gamma
+
+    trial_time = time.perf_counter() - start
+    print(f"Best gamma: {best_gamma}")
+    print(f"Best ARI: {best_score}")
+    print(f"Trial time: {trial_time:.2f} seconds")
+
+    final_run = 5
+    results = []
+
+    for i in range(final_run):
+        same_seeds(int(best_gamma) + i)
+        ari, nmi, acc = run_experiment(best_gamma, affine_model, cluster, labels, test)
+        results.append((acc, nmi, ari))
+
+    results = np.array(results)
+
+    endtime = time.perf_counter() - start
+    print("ARI: {:.4f} ± {:.4f}".format(results[:, 0].mean(), results[:, 0].std()))
+    print("NMI: {:.4f} ± {:.4f}".format(results[:, 1].mean(), results[:, 1].std()))
+    print("ACC: {:.4f} ± {:.4f}".format(results[:, 2].mean(), results[:, 2].std()))
+    print(f"Best gamma: {best_gamma}")
+    print("Total time: {:.2f} seconds".format(endtime))
+    print("\n")
+    return results, endtime
